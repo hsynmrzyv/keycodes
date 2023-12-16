a491e8c180b94d7740c7fa273f78b0aa9d9bd0b7
HSYN <huseyn.mirzayev@code.edu.az>
2023-12-17 01:13:47 +0400

    finish task :)

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const insertElement = document.getElementById("insert");
+
+window.addEventListener("keydown", (event) => {
+  console.log(event);
+  insertElement.innerHTML = `
+      <div class="key">
+        ${event.key}
+         <small>event.key</small>
+       </div>
+
+       <div class="key">
+         ${event.keyCode}
+         <small>event.keyCode</small>
+       </div>
+
+       <div class="key">
+        ${event.code}
+         <small>event.code</small>
+       </div>`;
+});
